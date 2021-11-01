8446dd3c2988bd62dec2154cb44e897f7bbea14f
riyaa-aa <riya.maiya22@gmail.com>
2021-11-01 21:40:53 +0800

    deleting unnecessary settings code

--- meals/forms.py
+++ meals/forms.py
@@ -6,27 +6,9 @@ class WeightTracker(forms.Form):
     dailyweight = forms.FloatField()
 
 class Restrictions(forms.ModelForm):
-    # RESTRICTIONS = [
-    #     ('vegan', 'vegan'),
-    #     ('vegetarian', 'vegetarian'),
-    #     ('pescatarian', 'pescatarian'),
-    #     ('gluten allergy', 'gluten allergy'),
-    #     ('lactose intolerant', 'lactose intolerant'),
-    #     ('nut allergy', 'nut allergy'),
-    # ]
-    # dietRestr = forms.MultipleChoiceField(
-    #     required=False,
-    #     widget=forms.CheckboxSelectMultiple,
-    #     choices=RESTRICTIONS,)
-    
     class Meta:
         model = UserInfo
         fields = ['restrictions']
         widgets = {
             'restrictions':forms.CheckboxSelectMultiple()
         }
-    
-    # customising the widget
-    # def __init__(self, *args, **kwargs): # overriding a function inhereted from the class
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['restrictions'].widget = forms.CheckboxSelectMultiple
